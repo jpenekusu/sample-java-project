f9369bbac8a1e0435000118c6e5e3070174afd41
jpenekusu <jpenekusu@gmail.com>
2017-11-23 23:20:27 +0100

    Update SampleJavaProjectTest.java

--- test/sample/java/project/SampleJavaProjectTest.java
+++ test/sample/java/project/SampleJavaProjectTest.java
@@ -32,6 +32,414 @@ public class SampleJavaProjectTest {
         assertEquals("foo", sjp.getName());
     }
 
+    @Test
+    public final void testGetSet1() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+
+    @Test
+    public final void testGetSet2() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+
+
+    @Test
+    public final void testGetSet3() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet4() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet5() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet6() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet7() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet8() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet9() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet10() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet11() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet12() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet13() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet14() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet15() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet16() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet17() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet18() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet19() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet20() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet21() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet22() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet23() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet24() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet25() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet26() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet27() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet28() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet29() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet30() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet31() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet32() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet33() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet34() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet35() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet36() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet37() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet38() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet39() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet40() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet41() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet42() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet43() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet44() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet45() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet46() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet47() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet48() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet49() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet50() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet51() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet52() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet53() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet54() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet55() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet56() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet57() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet58() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet59() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet60() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet61() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet62) {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet63() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet64() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet65() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet66() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet67() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet68() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet69() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet70() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet71() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet72() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet73() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet74() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet75() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet76() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet77() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet78() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet79() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet80() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }
+    @Test
+    public final void testGetSet81() {
+        sjp.setName("foo");
+        assertEquals("foo", sjp.getName());
+    }	    
     /**
      * Tests that the null check in the setter.
      */
